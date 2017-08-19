9c9d9478321080f7014dbf0b90a8a92867c6ec52
Cristian OV :v <cristianov19@gmail.com>
2017-08-18 21:34:39 -0600

    nuevo

---
+++ Modificaciones Cristian/ffff.py
@@ -0,0 +1,24 @@
+class Id:
+    def __init__(self,id):
+        self.id=id
+    def __str__(self):
+        return "id: {}".format(self.id)
+
+
+lista = []
+iddd = Id(123)
+lista.append(iddd)
+iddd = Id(321)
+lista.append(iddd)
+iddd = Id(345)
+lista.append(iddd)
+v=""
+ced = int(input("cedula: "))
+for x in lista:
+    if ced == x.id:
+        print("Yeah")
+        break
+    if ced != x.id:
+        v = False
+if v == False:
+    print("id no esta en lista")
